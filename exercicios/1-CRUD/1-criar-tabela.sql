ccaeb010a604e24d4ec30b32992eda92a2059d5d
Mateus <mcoeelho@gmail.com>
2025-08-01 04:20:21 +0000

    r: 1 e 2 (CRUD)

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,1 +1,5 @@
 -- Crie uma nova tabela no banco de dados
+CREATE TABLE salario_classe (
+id_salario INTEGER PRIMARY KEY AUTOINCREMENT,
+nivel TEXT,
+salario INTEGER);
--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,12 @@
 -- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
 
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+
+ALTER TABLE salario_classe
+DROP nivel;
+
+ALTER TABLE salario_classe
+ADD nivel varchar(50);
+
+ALTER TABLE employees
+ADD id_salario INTEGER;
