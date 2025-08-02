191d3786dee7de4dafc347dc7cc5c52dd000c1c6
Mateus <mcoeelho@gmail.com>
2025-08-02 06:27:27 +0000

    r: ALL (consultando-dados-varias-tabelas)

--- exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
+++ exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
@@ -1,1 +1,6 @@
 -- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
+
+SELECT *
+FROM albums al
+LEFT JOIN artists ar
+ON al.ArtistId = ar.ArtistId;
--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -2,3 +2,33 @@
 
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+
+SELECT
+t.TrackId id,
+t.Name musica,
+al.Title album,
+ar.Name artista
+FROM
+tracks t
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+INNER JOIN artists ar ON ar.ArtistId = al.ArtistId;
+
+
+WITH tabela_teste AS (
+SELECT
+t.TrackId id,
+t.Name musica,
+al.Title album,
+ar.Name artista
+FROM
+tracks t
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+INNER JOIN artists ar ON ar.ArtistId = al.ArtistId)
+
+SELECT
+artista,
+COUNT(musica) as quantidade_total_musicas
+FROM
+tabela_teste
+WHERE
+artista LIKE 'Caetano%';
